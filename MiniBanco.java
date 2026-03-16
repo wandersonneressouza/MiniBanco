0d3aaf621875b5a7f949f6ea777f7997ac3c4b78
wandersonneressouza <djandersonneres@hotmail.com>
2026-03-16 20:13:14 -0300

    implementar loop principal e roteamento de opçoes

--- MiniBanco.java
+++ MiniBanco.java
@@ -2,37 +2,64 @@ import java.util.Scanner;
 
 public class MiniBanco{
 
-    public static void exibirMenu(){
-        System.out.println("\n === Mini Banco ===");
+         static void exibirMenu(){
+        System.out.println("\n=== MiniBanco ===");
         System.out.println("1 - Depositar");
         System.out.println("2 - Sacar");
         System.out.println("3 - Consultar Saldo");
-        System.out.println("4 - Ver Estrato");
+        System.out.println("4 - Ver Extrato");
         System.out.println("0 - Sair");
-        System.out.print("Escolha: ");
+        System.out.println("Escolha: ");
     }
-
-
-
+    
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); 
-         
-        System.out.println("MiniBanco iniciado");
+        Scanner scanner = new Scanner(System.in);
 
-        //variaveis principal da conta;
-        double  saldo   = 0.0;
-        int     opcao   = -1;
+        // variaveis principais
 
-        //boas vindas
+        double saldo = 0;
+        int    opcao = -1;
+        
+        // boas vindas
 
-        System.out.print("Digite seu nome: ");
+        System.out.print("Digite o seu nome : ");
         String nome = scanner.next();
-        System.out.printf("Ola, %s! Saldo inicial: R$ %.2f\n", nome,saldo);
+        System.out.printf("Ola ,%s! saldo inicial : R$ %.2f\n", nome , saldo);
 
-        exibirMenu();
+
+        while (opcao !=0) {
+            exibirMenu();
+            opcao = scanner.nextInt();
+
+            if(opcao == 1 )
+            {
+                System.out.println(" [Depositar - em  breve]");
+            }
+            else if(opcao == 2 )
+            {
+                System.out.println(" [Sacar - em breve]");
+            }
+            else if( opcao == 3 )
+            {
+                System.out.println(" [Consultar Saldo]");
+            }
+            else if(opcao == 4)
+            {
+                System.out.println(" [Extrato em breve]");
+            }
+            else if(opcao == 0 )
+            {
+                System.out.println(" [Ate Logo, "+ nome + "!]");
+            }
+            else{
+
+                System.out.println(" [Opção invalida]");
+            }
+        }
+
+
 
         scanner.close();
-    
-    }
 
+    }
 }
